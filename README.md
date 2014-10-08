396f5c9ec69d86a858a3879a7adfbd412128c1bc
Fayland Lam <fayland@gmail.com>
2014-10-08 14:00:23 +0800

    from milla new

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Dancer::Logger::File::PerRequest - Blah blah blah
+
+# SYNOPSIS
+
+    use Dancer::Logger::File::PerRequest;
+
+# DESCRIPTION
+
+Dancer::Logger::File::PerRequest is
+
+# AUTHOR
+
+Fayland Lam <fayland@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2014- Fayland Lam
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Dancer/Logger/File/PerRequest.pm
@@ -0,0 +1,39 @@
+package Dancer::Logger::File::PerRequest;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Dancer::Logger::File::PerRequest - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Dancer::Logger::File::PerRequest;
+
+=head1 DESCRIPTION
+
+Dancer::Logger::File::PerRequest is
+
+=head1 AUTHOR
+
+Fayland Lam E<lt>fayland@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Fayland Lam
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
